7c0aede32d94a81cf38e80cbb3e71df151c36093
vsoch <vsoch@users.noreply.github.com>
2021-03-11 14:04:00 -0700

    adding more versions (and updated extraction using tags)
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- data/sourcecode/extract.sh
+++ data/sourcecode/extract.sh
@@ -2,6 +2,7 @@
 
 caliper extract github:LLNL/axom
 caliper extract github:Alpine-DAV/ascent
+caliper extract github:spack/spack
 caliper view github/LLNL-axom/changedlines/changedlines-results.json --title "LLNL axom Changed Lines" --outdir github/LLNL-axom/
 caliper view github/Alpine-DAV-ascent/changedlines/changedlines-results.json --title "Alpine-DAV ascent Changed Lines" --outdir github/Alpine-DAV-ascent/
 caliper view github/spack-spack/changedlines/changedlines-results.json --title "Spack Changed Lines" --outdir github/spack-spack
